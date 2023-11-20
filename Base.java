0917a00990883e2d6efce10439210392f4113d4b
Priyabrata-panda <150014064+Priyabrata-panda@users.noreply.github.com>
2023-11-20 12:47:04 +0530

    added base2 project

--- Base.java
+++ Base.java
@@ -1,11 +1,11 @@
 
-class Base1 {
+class Base2 {
     void display() {
         System.out.println("Derived");
     }
 }
 
-class Derived extends Base1 {
+class Derived extends Base2 {
     void display() {
         System.out.println("Base");
     }
@@ -19,7 +19,7 @@ class Raka extends Derived {
 
 public class Base{
     public static void main(String[] args) {
-        Base1 base = new Derived();
+        Base2 base = new Derived();
         base.display();
     }
 }
