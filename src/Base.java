5bb72eef7551cb0356c507d7e87d2dfb2aad51a1
Priyabrata-panda <150014064+priyabrata-panda@users.noreply.github.com>
2023-11-24 15:25:29 +0530

    added src

--- src/Base.java
+++ src/Base.java
@@ -2,25 +2,26 @@ package src;
 
 class Base3 {
     void display() {
-        System.out.println("src.Derived");
+        System.out.println("Derived");
     }
 }
 
 class Derived extends Base3 {
     void display() {
-        System.out.println("src.Base");
+
+        System.out.println("Base");
     }
 }
 class Raka extends Derived {
     void display() {
-        System.out.println("src.Raka");
+        System.out.println("Raka");
     }
 }
 
 
 public class Base{
     public static void main(String[] args) {
-        Base3 base = new Derived();
+        Base3 base = new Raka ();
         base.display();
     }
 }
